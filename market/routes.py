6aacc0a2f60ee6b2401d5ac699e8b157a2b452a0
nnikolic121201 <n.nikolic121201@gmail.com>
2026-02-21 22:45:01 +0100

    Dodao sam izmene

--- market/routes.py
+++ market/routes.py
@@ -5,3 +5,12 @@ from flask import render_template
 @app.route('/home')
 def home_page():
     return render_template('home.html')
+
+@app.route('/courses')
+def courses_page():
+    return render_template('courses.html', items=[])
+
+
+@app.route('/cart', methods=['GET', 'POST'])
+def cart_page():
+    return render_template('cart.html', cart_items=[], total_price=0)
